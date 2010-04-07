c08699064c96d0589ba9fc8145bb14827bd44e2c
sam.silverberg <sam.silverberg@856e77c2-2ed6-11df-8e64-fd3be38ee323>
2010-04-07 20:03:50 +0000

    0.8.14 Release

---
+++ tags/0.8.14/src/org/opendedup/sdfs/filestore/gc/ChunkClaimJob.java
@@ -0,0 +1,22 @@
+package org.opendedup.sdfs.filestore.gc;
+
+import java.io.IOException;
+
+import org.opendedup.sdfs.servers.HashChunkService;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class ChunkClaimJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext ctx) throws JobExecutionException {
+		try {
+			HashChunkService.processHashClaims();
+			HashChunkService.removeStailHashes();
+		} catch (Exception e) {
+			throw new JobExecutionException(e);
+		}
+	}
+
+}
