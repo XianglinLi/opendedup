423e4f2d7ee32208b923809cd56435347dc3bd58
sam.silverberg <sam.silverberg@gmail.com>
2013-03-27 16:09:41 +0000

    Version 1.2.4

---
+++ org/opendedup/sdfs/servers/SDFSService.java
@@ -0,0 +1,133 @@
+package org.opendedup.sdfs.servers;
+
+import java.io.File;
+
+
+import org.opendedup.logging.SDFSLogger;
+import org.opendedup.sdfs.Config;
+import org.opendedup.sdfs.Main;
+import org.opendedup.sdfs.filestore.DedupFileStore;
+import org.opendedup.sdfs.filestore.MetaFileStore;
+import org.opendedup.sdfs.filestore.gc.SDFSFDiskScheduler;
+import org.opendedup.sdfs.filestore.gc.StandAloneGCScheduler;
+import org.opendedup.sdfs.mgmt.MgmtWebServer;
+import org.opendedup.sdfs.network.NetworkDSEServer;
+import org.opendedup.sdfs.notification.SDFSEvent;
+import org.opendedup.util.OSValidator;
+
+public class SDFSService {
+	String configFile;
+
+	private SDFSFDiskScheduler gc = null;
+	private String routingFile;
+	private NetworkDSEServer ndServer = null;
+
+	public SDFSService(String configFile, String routingFile) {
+
+		this.configFile = configFile;
+		this.routingFile = routingFile;
+		System.out.println("Running SDFS Version " + Main.version);
+		if (routingFile != null)
+			SDFSLogger.getLog().info(
+					"reading routing config file = " + this.routingFile);
+		System.out.println("reading config file = " + this.configFile);
+	}
+
+	public void start() throws Exception {
+
+		Config.parseSDFSConfigFile(this.configFile);
+		MgmtWebServer.start();
+		Main.mountEvent = SDFSEvent.mountEvent("SDFS Version [" + Main.version
+				+ "] Mounting Volume from " + this.configFile);
+		if (this.routingFile != null)
+			Config.parserRoutingFile(routingFile);
+		else if (!Main.chunkStoreLocal) {
+			Config.parserRoutingFile(OSValidator.getConfigPath()
+					+ File.separator + "routing-config.xml");
+		}
+		try {
+			if (Main.volume.getName() == null)
+				Main.volume.setName(configFile);
+			Main.volume.setClosedGracefully(false);
+			Config.writeSDFSConfigFile(configFile);
+		} catch (Exception e) {
+			SDFSLogger.getLog().error("Unable to write volume config.", e);
+		}
+		HCServiceProxy.init();
+		if (Main.chunkStoreLocal) {
+			try {
+				
+				if (Main.enableNetworkChunkStore && !Main.runCompact) {
+					ndServer = new NetworkDSEServer();
+					new Thread(ndServer).start();
+				}
+			} catch (Exception e) {
+				SDFSLogger.getLog().error("Unable to initialize volume ", e);
+				System.err.println("Unable to initialize Hash Chunk Service");
+				e.printStackTrace();
+				System.exit(-1);
+			}
+			if (Main.runCompact) {
+				this.stop();
+				System.exit(0);
+			}
+
+			Main.pFullSched= new StandAloneGCScheduler();
+		}
+
+		if (!Main.chunkStoreLocal) {
+			gc = new SDFSFDiskScheduler();
+		}
+		Main.mountEvent.endEvent("Volume Mounted");
+	}
+
+	public void stop() {
+		SDFSEvent evt = SDFSEvent.umountEvent("Unmounting Volume");
+		SDFSLogger.getLog().info("Shutting Down SDFS");
+		SDFSLogger.getLog().info("Stopping FDISK scheduler");
+		if (!Main.chunkStoreLocal) {
+			gc.stopSchedules();
+		} else {
+			try {
+				Main.pFullSched.close();
+				Main.pFullSched = null;
+			}catch(Exception e) {}
+		}
+		SDFSLogger.getLog().info("Flushing and Closing Write Caches");
+		DedupFileStore.close();
+		SDFSLogger.getLog().info("Write Caches Flushed and Closed");
+		SDFSLogger.getLog().info("Committing open Files");
+		MetaFileStore.close();
+		SDFSLogger.getLog().info("Open File Committed");
+		SDFSLogger.getLog().info("Writing Config File");
+
+		/*
+		 * try { MD5CudaHash.freeMem(); } catch (Exception e) { }
+		 */
+		MgmtWebServer.stop();
+		try {
+			Process p = Runtime.getRuntime().exec(
+					"umount " + Main.volumeMountPoint);
+			p.waitFor();
+		} catch (Exception e) {
+		}
+		if (Main.chunkStoreLocal) {
+			SDFSLogger.getLog().info(
+					"######### Shutting down HashStore ###################");
+			HCServiceProxy.close();
+			if (Main.enableNetworkChunkStore && !Main.runCompact) {
+				ndServer.close();
+			} else {
+
+			}
+		}
+		try {
+			Main.volume.setClosedGracefully(true);
+			Config.writeSDFSConfigFile(configFile);
+		} catch (Exception e) {
+			SDFSLogger.getLog().error("Unable to write volume config.", e);
+		}
+		evt.endEvent("Volume Unmounted");
+		SDFSLogger.getLog().info("SDFS is Shut Down");
+	}
+}
---
+++ org/opendedup/util/LongCollisionTester.java
@@ -0,0 +1,9 @@
+package org.opendedup.util;
+
+public class LongCollisionTester {
+	public static void main(String[] args) {
+		System.out.println("Hash value for 12746371072 = " + Long.valueOf(12746371072L).hashCode());
+		System.out.println("Hash value for 634880 = " + Long.valueOf(634880L).hashCode());
+	}
+
+}
